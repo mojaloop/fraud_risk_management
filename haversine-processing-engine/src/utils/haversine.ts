533069ada4e112a444ee0b6cc33434fce755db49
Aaron Reynoza <ldaaron38@gmail.com>
2020-11-25 22:00:48 -0600

    more linting

--- haversine-processing-engine/src/utils/haversine.ts
+++ haversine-processing-engine/src/utils/haversine.ts
@@ -9,11 +9,11 @@ const distance = function(location1: number[], location2: number[], useMiles = f
   const dLat: number = deg2rad(location2[0] - location1[0]);
   const dLon: number = deg2rad(location2[1] - location1[1]);
   const a: number = Math.sin(dLat / 2) * 
-                  Math.sin(dLat / 2) + 
-                  Math.cos(deg2rad(location1[0])) * 
-                  Math.cos(deg2rad(location2[0])) * 
-                  Math.sin(dLon / 2) * 
-                  Math.sin(dLon / 2);
+                    Math.sin(dLat / 2) + 
+                    Math.cos(deg2rad(location1[0])) * 
+                    Math.cos(deg2rad(location2[0])) * 
+                    Math.sin(dLon / 2) * 
+                    Math.sin(dLon / 2);
   const c: number = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   let d: number = Math.round(coreRadius * c);
   if (useMiles) {	// returns miles instead of kilometers
