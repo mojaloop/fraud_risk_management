4db0286b0174bb929101bebbdd0e25d21ef9ae5c
jfoley <johannes.foley@sybrin.com>
2020-11-26 16:37:48 +0200

    code cleanup.

--- blocklist-processing-engine/src/consumers/party-consumer.ts
+++ blocklist-processing-engine/src/consumers/party-consumer.ts
@@ -3,16 +3,14 @@ import { isBlocked } from "../redis-client/redis-client";
 import { log, sanitizeNumber } from '../helper';
 
 const getMSISDN = (message: string): string => {
-    // TODO parse message as json and get the msisdn
-    const msisdn = sanitizeNumber(message);
-    return msisdn;
+    const jMessage = JSON.parse(message);
+    return sanitizeNumber(jMessage.msisdn);
 }
 
 const handleMessage = async (message: kafka.Message, topic: string, handleBlock: (msisdn: string, topic: string, isBlocked: number) => any) => {
     const msisdn = getMSISDN(message.value.toString());
     const blocked = await isBlocked(msisdn);
     await handleBlock(msisdn, topic, blocked);
-    log('Message Processed ' + new Date().toISOString(), topic);
 }
 
 export { handleMessage }
--- blocklist-processing-engine/src/consumers/quote-consumer.ts
+++ blocklist-processing-engine/src/consumers/quote-consumer.ts
@@ -3,7 +3,6 @@ import { isBlocked } from "../redis-client/redis-client";
 import { log, sanitizeNumber } from '../helper';
 
 const handleMessage = async (message: kafka.Message, topic: string, handleBlock: (msisdn: string, topic: string, isBlocked: number) => any) => {
-    //log('New message received:\r\n' + message.value, topic);
     const msisdns = getMSISDNs(message.value.toString());
     let promises: Promise<any>[] = [];
 
@@ -14,17 +13,15 @@ const handleMessage = async (message: kafka.Message, topic: string, handleBlock:
             resolve();
         }));
     });
-    await Promise.all(promises).then(() => {
-        log('Message Processed ' + new Date().toISOString(), topic)
-    });
+    await Promise.all(promises);
 }
 
 const getMSISDNs = (message: string): string[] => {
-    let toReturn: string[] = [];
     const jMessage = JSON.parse(message);
-    toReturn.push(sanitizeNumber(jMessage.payer.partyIdInfo.partyIdentifier));
-    toReturn.push(sanitizeNumber(jMessage.payee.partyIdInfo.partyIdentifier));
-    return toReturn;
+    return [
+        sanitizeNumber(jMessage.payer.partyIdInfo.partyIdentifier),
+        sanitizeNumber(jMessage.payee.partyIdInfo.partyIdentifier)
+    ]
 }
 
 export { handleMessage }
