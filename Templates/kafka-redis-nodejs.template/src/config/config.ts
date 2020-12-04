0e365b8782aef03352a4dd5244c39c769e7e7680
jfoley <johannes.foley@sybrin.com>
2020-12-04 10:58:16 +0200

    Added a new template that subscribes to a Kafka topic, reads some value from Redis, logs to Kafka and posts a result to Kafka.

---
+++ Templates/kafka-redis-nodejs.template/src/config/config.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import { config as dotenv } from 'dotenv';
+
+// Load .env file into process.env if it exists. This is convenient for running locally.
+dotenv({
+  path: path.resolve(__dirname, '../../.env'),
+});
+
+const configuration: ConfigObj = {
+  kafkaEndpoint: <string>process.env.KAFKA_ENDPOINT,
+  topics: (<any>process.env.TOPICS).split(','),
+  resultTopic: <string>process.env.RESULT_TOPIC,
+  partition: parseInt(<any>process.env.PARTITION, 10),
+  autoCommit: <any>process.env.AUTO_COMMIT,
+  redisHost: <string>process.env.REDIS_HOST,
+  redisPort: <any>process.env.REDIS_PORT,
+  redisDB: <any>process.env.REDIS_DB,
+  logTopic: <string> process.env.LOG_TOPIC,
+};
+
+interface ConfigObj {
+  kafkaEndpoint: string;
+  topics: string[],
+  resultTopic: string,
+  partition: number,
+  autoCommit: boolean,
+  redisHost: string,
+  redisPort: number,
+  redisDB: number,
+  logTopic:string
+}
+
+export { configuration, ConfigObj };
