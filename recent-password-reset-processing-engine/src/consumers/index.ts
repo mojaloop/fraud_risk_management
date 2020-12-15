55abdab4ab198fb1785802be75cb9745b35d2077
jfoley <johannes.foley@sybrin.com>
2020-12-15 12:39:02 +0200

    Added new recent password change processing engine.

---
+++ recent-password-reset-processing-engine/src/consumers/index.ts
@@ -0,0 +1,43 @@
+import * as kafka from 'kafka-node';
+import { ConfigObj } from '../config/config';
+import { log } from '../helper';
+import handleQuoteMessage from './quote-consumer';
+
+const createConsumer = (topic: string, config: ConfigObj) => new kafka.Consumer(
+  new kafka.KafkaClient({ kafkaHost: config.kafkaEndpoint }),
+  [{
+    topic,
+    partition: config.partition,
+  }],
+  { autoCommit: config.autoCommit },
+);
+
+const getMessageHandler = (topic: string) => {
+  switch (topic) {
+    case 'quotes':
+    default:
+      return handleQuoteMessage;
+  }
+};
+
+/**
+* Subscribe to the configured Kafka server
+* to the selected Kafka topic
+*/
+const createKafkaConsumer = (topic: string, config: ConfigObj) => {
+  log('Starting Processing Engine...', topic);
+  try {
+    const consumer = createConsumer(topic, config);
+    const handleMessage = getMessageHandler(topic);
+
+    consumer.on('message', async (message: kafka.Message) => {
+      await handleMessage(message, topic);
+    });
+
+    log('Started Processing Engine.', topic);
+  } catch (e) {
+    log(`Unhandled exception while starting consumer with details: ${e}`, topic);
+  }
+};
+
+export default createKafkaConsumer;
