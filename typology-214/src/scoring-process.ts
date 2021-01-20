26e8f590964abbd49763fab2c013d75eccab68c4
jfoley <johannes.foley@sybrin.com>
2021-01-20 16:00:41 +0200

    Added scoring to typology 214; Added rule 48 to Typology 214;

--- typology-214/src/scoring-process.ts
+++ typology-214/src/scoring-process.ts
@@ -9,29 +9,31 @@ import { get } from './redis-client';
 import { log } from './helper';
 
 class typology214Type {
-  rule17: boolean | undefined;
+  rule3: boolean | undefined;
+  rule12: boolean | undefined;
   rule27: boolean | undefined;
-  rule86: boolean | undefined;
-  rule87: boolean | undefined;
+  rule30: boolean | undefined;
+  rule48: boolean | undefined;
+  rule78: boolean | undefined;
 }
 
 // Composed probability for typology 214 = (012.p+027.p+030.p+048.p+078.p)
 const handleScores = (scores: any, topic: string, TransactionID: string) => {
   const score =
-    (scores.rule12 ? 0.14 : 0)
-    + (scores.rule27 ? 0.14 : 0)
-    + (scores.rule30 ? 0.14 : 0)
-    + (scores.rule63 ? 0.15 : 0)
-    + (scores.rule64 ? 0.15 : 0)
-    ;
+    (scores.rule3 ? 0 : 0)
+    + (scores.rule12 ? 0.2 : 0)
+    + (scores.rule27 ? 0.2 : 0)
+    + (scores.rule30 ? 0.2 : 0)
+    + (scores.rule48 ? 0.2 : 0)
+    + (scores.rule78 ? 0.2 : 0);
 
-  publish(topic, `"typology":"typology-214","transactionID":"${TransactionID}","score":"${score}","textResult":"Typology 214 score is ${score}, Reason: ${(scores.rule2 ? 'Velocity (incoming), ' : '')
+  publish(topic, `"typology":"typology-214","transactionID":"${TransactionID}","score":"${score}","textResult":"Typology 214 score is ${score}, Reason: ${
+    (scores.rule3 ? 'Account Dormancy - Payee, ' : '')
     + (scores.rule12 ? 'Party Type Individual, ' : '')
-    + (scores.rule16 ? 'Transaction Convergence, ' : '')
     + (scores.rule27 ? 'Transaction Mirroring, ' : '')
     + (scores.rule30 ? 'New Payee, ' : '')
-    + (scores.rule63 ? "Benford's Law, " : '')
-    + (scores.rule64 ? 'Uniform Distribution - Payee' : '')
+    + (scores.rule48 ? 'Large Transaction - Payer, ' : '')
+    + (scores.rule78 ? 'Cash Withdrawel' : '')
     + '"}'
     }`);
 }
@@ -55,18 +57,24 @@ const handleQuoteMessage = async (
     // See https://lextego.atlassian.net/browse/ACTIO-199
     const scores: typology214Type = new typology214Type();
 
-    try { scores.rule17 = rules.handleTransactionDivergence(transfer, payeeHistoricalSendData); }
-    catch (error) {
-      log(`Error while handling transaction divergence for ${TransactionID}, with message: \r\n${error}`, topic)
-    }
+    // rule 17 not part of typo 214
+    // try { scores.rule17 = rules.handleTransactionDivergence(transfer, payeeHistoricalSendData); }
+    // catch (error) {
+    //   log(`Error while handling transaction divergence for ${TransactionID}, with message: \r\n${error}`, topic)
+    // }
     try { scores.rule27 = rules.handleTransactionMirroring(transfer, payeeHistoricalSendData, payeeHistoricalReceiveData); }
     catch (error) {
       log(`Error while handling Transaction Mirroring ${TransactionID}, with message: \r\n${error}`, topic)
     }
-    try { scores.rule86 = await rules.handleTransactionsBetweenParties(transfer, senderClient, sourceHistoricalSendData); }
+    try { scores.rule48 = await rules.handleLargeTransactionPayer(transfer, sourceHistoricalSendData); }
     catch (error) {
-      log(`Error while handling Transaction Mirroring ${TransactionID}, with message: \r\n${error}`, topic)
+      log(`Error while handling Large Transaction Payer ${TransactionID}, with message: \r\n${error}`, topic)
     }
+
+    // try { scores.rule86 = await rules.handleTransactionsBetweenParties(transfer, senderClient, sourceHistoricalSendData); }
+    // catch (error) {
+    //   log(`Error while handling Transaction Mirroring ${TransactionID}, with message: \r\n${error}`, topic)
+    // }
 
     handleScores(scores, topic, TransactionID);
   } catch (e) {
