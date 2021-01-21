f7144188e15b6246446aa932e85a12c87810bc17
Aaron Reynoza <ldaaron38@gmail.com>
2021-01-20 23:48:34 -0600

    add transaction convergence rule

--- typology-28/src/rules/index.ts
+++ typology-28/src/rules/index.ts
@@ -1,9 +1,11 @@
 import handleTransactionMirroring from './transaction-mirroring';
 import handleTransactionDivergence from './transaction-divergence';
+import handleTransactionConvergence from './transaction-convergence';
 import handleBenfordsLaw from './benfordsLaw'
 
 export default {
   handleTransactionMirroring,
   handleTransactionDivergence,
+  handleTransactionConvergence,
   handleBenfordsLaw,
 }
