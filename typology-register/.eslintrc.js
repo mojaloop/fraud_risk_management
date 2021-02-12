6d2d9aaf4d16fed97bb4a0fb60ef342a333a4660
Yusuf Göksu <me@yusufgoksu.com>
2021-02-12 19:15:09 +0300

    revert the eslint rule change

--- typology-register/.eslintrc.js
+++ typology-register/.eslintrc.js
@@ -27,6 +27,5 @@ module.exports = {
     'linebreak-style': ['error', 'unix'],
     semi: ['error', 'always'],
     'comma-dangle': ['error', 'always-multiline'],
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
