2cb26cf111d578882d6b1bd50cd09afdfe895d2a
Joey Goksu <me@yusufgoksu.com>
2021-05-05 16:55:45 +0300

    create interface folder

---
+++ Templates/functional-template/src/interfaces/iConfig.ts
@@ -0,0 +1,19 @@
+export interface IConfig {
+  port: number;
+  redisDB: string;
+  redisAuth: string;
+  redisHost: string;
+  redisPort: number;
+  apmLogging: boolean;
+  redisConnection: boolean;
+  apmServiceName: string;
+  apmSecretToken: string;
+  apmURL: string;
+  dbName: string;
+  dbURL: string;
+  dbUser: string;
+  dbPassword: string;
+  graphName: string;
+  collectionName: string;
+  dev: string;
+}
