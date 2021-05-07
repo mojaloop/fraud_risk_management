45aaa8d838aa4cf68929c45e3a9a2f388e87bd3c
Joey Goksu <me@yusufgoksu.com>
2021-05-07 13:24:58 +0300

    update the route file

--- payment-platform-adapter/src/routes/index.ts
+++ payment-platform-adapter/src/routes/index.ts
@@ -1,5 +1,5 @@
 import Router from 'koa-router';
-import * as miscController from './controllers/misc';
+import * as miscController from '../controllers/misc';
 const router = new Router();
 
 router.get('/', miscController.healthcheck);
