98111ad9e6f249f1da49db01ca988cc94ff94c85
Aaron Reynoza <aaron.alejandro.reynoza@gmail.com>
2021-05-11 23:55:55 -0600

    add transaction-aggregation-decisioning processor

---
+++ transaction-aggregation-decisioning-processor/src/interfaces/iConfig.ts
@@ -0,0 +1,23 @@
+export interface IConfig {
+  functionName: string;
+  port: number;
+  redisDB: string;
+  redisAuth: string;
+  redisHost: string;
+  redisPort: number;
+  apmLogging: boolean;
+  redisConnection: boolean;
+  apmServiceName: string;
+  apmSecretToken: string;
+  apmURL: string;
+  dbName: string;
+  dbURL: string;
+  dbUser: string;
+  dbPassword: string;
+  graphName: string;
+  collectionName: string;
+  dev: string;
+  transactionRoutingHostname: string;
+  transactionRoutingPort: number;
+  transactionRoutingPath: string;
+}
