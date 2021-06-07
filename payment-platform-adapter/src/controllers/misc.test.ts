fc1a71ef5ba3021dbef5322d5d108f95615f973b
jfoley <johannes.foley@sybrin.com>
2021-06-07 19:06:53 +0200

    cleanup

--- payment-platform-adapter/src/controllers/misc.test.ts
+++ payment-platform-adapter/src/controllers/misc.test.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { Context } from 'koa';
 import { configuration } from '../config';
 import { CustomerCreditTransferInitiation } from '../interfaces/iFRMTransaction';
-import { iMLTransaction } from '../interfaces/iMLTransaction';
 import { healthcheck, monitorTransaction } from './misc';
 
 describe('test misc functions', () => {
@@ -29,7 +28,6 @@ describe('test misc functions', () => {
 
   test('should monitorTransaction for PAYEE result is VALID', async () => {
     const mlTran = JSON.parse(
-      
       '{  "quoteId": "ABC123",  "transactionId": "asdf1234",  "transactionRequestId": "string",  "payee": {    "partyIdInfo": {      "partyIdType": "MSISDN",      "partyIdentifier": "+27723748019",      "partySubIdOrType": "string",      "fspId": "bank1",      "extensionList": {        "extension": [          {            "key": "somekey",            "value": "somevalue"          }        ]      }    },    "merchantClassificationCode": "merchCode",    "name": "string",    "personalInfo": {      "complexName": {        "firstName": "payeefirstName",        "middleName": "payeemiddleName",        "lastName": "payeelastname"      },      "dateOfBirth": "2021-05-28"    }  },  "payer": {    "partyIdInfo": {      "partyIdType": "MSISDN",      "partyIdentifier": "+27723748020",      "partySubIdOrType": "string",      "fspId": "string",      "extensionList": {        "extension": [          {            "key": "string",            "value": "string"          }        ]      }    },    "merchantClassificationCode": "merchCode",    "name": "string",    "personalInfo": {      "complexName": {        "firstName": "payerfirstName",        "middleName": "payermiddleName",        "lastName": "payerlastname"      },      "dateOfBirth": "2021-05-28"    }  },  "amountType": "SEND",  "amount": {    "currency": "USD",    "amount": 123.45  },  "fees": {    "currency": "USD",    "amount": 12.34  },  "transactionType": {    "scenario": "DEPOSIT",    "subScenario": "string",    "initiator": "PAYEE",    "initiatorType": "CONSUMER",    "refundInfo": {      "originalTransactionId": "string",      "refundReason": "string"    },    "balanceOfPayments": "string"  },  "geoCode": {    "latitude": "string",    "longitude": "string"  },  "note": "string",  "expiration": "string",  "extensionList": {    "extension": [      {        "key": "string",        "value": "string"      }    ]  }}',
     );
     const ctx = {
@@ -42,7 +40,7 @@ describe('test misc functions', () => {
     const ctxTest = await monitorTransaction(ctx as Context);
     const expected = JSON.stringify(
       new CustomerCreditTransferInitiation(mlTran),
-    ); // .SupplementaryData["payer.merchantClassificationCode"].toString();
+    ); 
 
     expect(JSON.stringify(ctxTest.body)).toMatch(expected);
     axioPostSpy.mockRestore();
@@ -50,7 +48,6 @@ describe('test misc functions', () => {
 
   test('should monitorTransaction for PAYER result is VALID', async () => {
     const mlTran = JSON.parse(
-      
       '{  "quoteId": "ABC123",  "transactionId": "asdf1234",  "transactionRequestId": "string",  "payee": {    "partyIdInfo": {      "partyIdType": "MSISDN",      "partyIdentifier": "+27723748019",      "partySubIdOrType": "string",      "fspId": "bank1",      "extensionList": {        "extension": [          {            "key": "somekey",            "value": "somevalue"          }        ]      }    },    "merchantClassificationCode": "merchCode",    "name": "string",    "personalInfo": {      "complexName": {        "firstName": "payeefirstName",        "middleName": "payeemiddleName",        "lastName": "payeelastname"      },      "dateOfBirth": "2021-05-28"    }  },  "payer": {    "partyIdInfo": {      "partyIdType": "MSISDN",      "partyIdentifier": "+27",      "partySubIdOrType": "string",      "fspId": "string",      "extensionList": {        "extension": [          {            "key": "string",            "value": "string"          }        ]      }    },    "merchantClassificationCode": "merchCode",    "name": "string",    "personalInfo": {      "complexName": {        "firstName": "payerfirstName",        "middleName": "payermiddleName",        "lastName": "payerlastname"      },      "dateOfBirth": "2021-05-28"    }  },  "amountType": "SEND",  "amount": {    "currency": "USD",    "amount": 123.45  },  "fees": {    "currency": "USD",    "amount": 12.34  },  "transactionType": {    "scenario": "DEPOSIT",    "subScenario": "string",    "initiator": "PAYER",    "initiatorType": "CONSUMER",    "refundInfo": {      "originalTransactionId": "string",      "refundReason": "string"    },    "balanceOfPayments": "string"  },  "geoCode": {    "latitude": "string",    "longitude": "string"  },  "note": "string",  "expiration": "string",  "extensionList": {    "extension": [      {        "key": "string",        "value": "string"      }    ]  }}',
     );
     const ctx = {
@@ -62,7 +59,7 @@ describe('test misc functions', () => {
     const ctxTest = await monitorTransaction(ctx as Context);
     const expected = JSON.stringify(
       new CustomerCreditTransferInitiation(mlTran),
-    ); // .SupplementaryData["payer.merchantClassificationCode"].toString();
+    );
 
     expect(JSON.stringify(ctxTest.body)).toMatch(expected);
     axioPostSpy.mockRestore();
